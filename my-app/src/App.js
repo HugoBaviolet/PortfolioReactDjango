0a7fbf4dc3c055b8c1642efc868497264448edc8
Hugo A'Violet <hugoaviolet@Hugos-MacBook-Pro.local>
2022-03-01 20:42:43 +0000

    Macbook commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HUGO <code>src/App.js</code> and save to reload.
+          HUGO  is cool <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
