6febfa085bd2d546509bb8fe1ff26f7d6321dced
Hugo <hbaviolet@hotmail.co.uk>
2022-03-19 01:00:04 +0000

    Changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HUGO  is cool <code>src/App.js</code> and save to reload.
+          HUGO  is learning <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
